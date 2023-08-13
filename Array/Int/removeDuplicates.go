723b1613eb492c4ed79e21ed919014fa9f5baf79
haitwang <wanghaitao@shu.edu.cn>
2023-08-13 10:07:00 +0800

    add 删除有序数组中的重复项 II

--- Array/Int/removeDuplicates.go
+++ Array/Int/removeDuplicates.go
@@ -2,13 +2,15 @@ package Int
 
 // https://leetcode.cn/problems/remove-duplicates-from-sorted-array/description/?envType=study-plan-v2&envId=top-interview-150
 func removeDuplicates(nums []int) int {
-	slow, fast := 0, 0
-	for fast < len(nums) {
-		if nums[slow] != nums[fast] {
-			slow++
-			nums[slow] = nums[fast]
-		}
-		fast++
+	if len(nums) == 0 {
+		return 0
 	}
-	return slow + 1
+	slow := 1
+	for fast := 1; fast < len(nums); fast++ {
+		if nums[fast] != nums[fast-1] {
+			nums[slow] = nums[fast]
+			slow++
+		}
+	}
+	return slow
 }
