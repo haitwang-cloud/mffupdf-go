bd404f847e09defdc980ab588fd5de987ff0649a
haitwang <wanghaitao@shu.edu.cn>
2023-08-13 14:44:23 +0800

    add isPalindrome

---
+++ Array/twoPointers/Str/isPalindrome.go
@@ -0,0 +1,29 @@
+package Str
+
+import "strings"
+
+// https://leetcode.cn/problems/valid-palindrome/description/?envType=study-plan-v2&envId=top-interview-150
+func isPalindrome(s string) bool {
+	s = strings.ToLower(s)
+	left, right := 0, len(s)-1
+	for left < right {
+		for left < right && !isValidChar(s[left]) {
+			left++
+		}
+		for left < right && !isValidChar(s[right]) {
+			right--
+		}
+		if left < right {
+			if s[left] != s[right] {
+				return false
+			}
+			left++
+			right--
+		}
+	}
+	return true
+}
+
+func isValidChar(ch byte) bool {
+	return (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9')
+}
--- Array/twoPointers/Str/strStr.go
+++ Array/twoPointers/Str/strStr.go
@@ -6,16 +6,13 @@ func strStr(haystack string, needle string) int {
 		return 0
 	}
 
-	// 从 haystack 的开头开始遍历。
 	for i := 0; i < len(haystack)-len(needle)+1; i++ {
-		// 如果 haystack 的第 i 个字符与 needle 的第一个字符相等，则从第 i + 1 个字符开始比较。
 		if haystack[i] == needle[0] {
 			j := 1
 			for j < len(needle) && haystack[i+j] == needle[j] {
 				j++
 			}
-			// 如果比较了 needle 的全部字符，则返回 i。
-			if len(needle) == j {
+			if j == len(needle) {
 				return i
 			}
 		}
